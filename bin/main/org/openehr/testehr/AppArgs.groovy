91e615c8595ccd42ede9cbe069071760d47fda5f
ppazos <pablo.swp@gmail.com>
2020-06-13 15:13:28 -0300

    added test cases for multiple ehrs

---
+++ bin/main/org/openehr/testehr/AppArgs.groovy
@@ -0,0 +1,10 @@
+package org.openehr.testehr
+
+import groovy.cli.Option
+
+interface AppArgs {
+    @Option(description='number of EHRs to create') int ehrs()
+    @Option(description='operational template file location') String template()
+    @Option(description='number of compositions to commit') int compositions()
+    @Option(description='AQL request body file location') String aql()
+}
